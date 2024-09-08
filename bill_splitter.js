89466409c1fb3697c50604869c9018bcff82162f
jenna0315 <jbellagamba@usf.edu>
2024-09-08 16:12:49 -0400

    Initial Logic for Calculating the Tip

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,1 +1,4 @@
-
+//Initial Logic for Calculating the Tip
+let customerBill = 295 //number
+let customertip = (300 < customerBill < 50) ? (.2*customerBill) : (.15*customerBill) 
+console.log("Initial Customer Tip:",customertip)
